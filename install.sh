657d55a90873c853dfd0a3324b2a6493bc83b140
mintoya <119896751+mintoya@users.noreply.github.com>
2025-08-28 20:14:48 -0400

    install script slight upgrade

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-yay -S --noconfirm hyprland nushell swww matugen quickshell rofi blueman upower jc NetworkManager nmgui ghostty neovim yazi zoxide 
+yay -S --noconfirm hyprland nushell swww matugen quickshell rofi blueman upower jc NetworkManager nmgui ghostty-git neovim yazi zoxide brightnessctl
